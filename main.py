965a93e0b97a12ea9c03559437bc533044475949
peerpressurre <gsophiay@gmail.com>
2022-09-29 23:46:43 +0300

    Update: feature:hm_ex_5

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 time = int(input('Enter current time->'))
-res = ''
 
-if time <6:
-    res =
-
+if 0 < time <6:
+    print('Good nigh')
+elif time == 6:
+    print('Good morning')
+elif 6 < time < 13:
+    print('Good morning')
+elif time == 13:
+    print('Good day')
+elif 13 < time < 17:
+    print('Good day')
+elif time == 17:
+    print('Good evening')
+elif 17 < time < 24:
+    print('Good evening')
+elif time == 24:
+    print('Good evening')
+else:
+    print('Error')
