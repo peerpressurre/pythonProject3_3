f9f069596921497dfc559e0141a040a6d5fb5ead
student vova <popihacvova@gmail.com>
2022-09-29 20:33:24 +0300

    update: feature/hm_ex_3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ p = int(input('enter price->'))
 q = int(input('enter quantity->'))
 d = int(input('enter discount->'))
 
-print ('| sum -> summary\n| pwd -> price of one console considering discount')
+print('| sum -> summary\n| pwd -> price of one console considering discount')
 
 op = input('->')
 res = ''
@@ -12,6 +12,5 @@ if op == 'sum':
     res = int(sum1-((d*sum1)/100))
 elif op == 'pwd':
     res = int(p-((d*p)/100))
-
-    print(f'res = {res}')
+print(f'res = {res}')
 
