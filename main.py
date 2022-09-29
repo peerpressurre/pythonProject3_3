1471899ec2eba508f5bb665b4a54b62d4633c3a6
student vova <popihacvova@gmail.com>
2022-09-29 20:47:24 +0300

    update: feature/hm_ex_1

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ between = end - sec
 h = 0
 min = 0
 
- print (f'Your time: {int(sec/3600)}:{int((sec%3600)/60)}:{int((sec%3600)%60)}')
- if between > 0
+print (f'Your time: {int(sec/3600)}:{int((sec%3600)/60)}:{int((sec%3600)%60)}')
+if between > 0:
   h = int(between/3600)
   min = int((between%3600)/60)
   sec = int((between%3600)%60)
   print (f'Time left: {h}:{min}:{sec}')
- else:
+else:
   print('Error!')
